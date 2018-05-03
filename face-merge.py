5388eec8c3372e40baa3fb87d373c8840a83f862
HuShiyu <hushiyu1995@qq.com>
2018-05-03 12:15:20 +0800

    change

--- face-merge.py
+++ face-merge.py
@@ -140,12 +140,20 @@ if __name__ == '__main__':
     print(line.split()[31])
     print(line.split()[33])
     img  = []
+    # while line:
+    #     array = line.split()
+    #     if (array[9] == int(Blond_Hair)): 
+    #         if (array[20] == int(Male): 
+    #             if (array[31] == int(Smiling):
+    #                 if (array[33] == int(Wavy_Hair)):
+    #                     img.append(array[0])
+    #     line = f.readline()
     while line:
-	    array = line.split()
-        if (array[9] == int(Blond_Hair)): 
-            if (array[20] == int(Male): 
-                if (array[31] == int(Smiling):
-                    if (array[33] == int(Wavy_Hair)):
+        array = line.split()
+        if (array[9] == "int(Blond_Hair)"): 
+            if (array[20] == "int(Male)": 
+                if (array[31] == "int(Smiling)":
+                    if (array[33] == "int(Wavy_Hair)"):
                         img.append(array[0])
         line = f.readline()
 
